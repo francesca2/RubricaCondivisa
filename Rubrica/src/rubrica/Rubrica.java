df20099356acb4ea5e84df24905c105e741c418d
viviana <viviana_1993@live.it>
2016-10-03 16:11:09 +0200

    System.out

--- Rubrica/src/rubrica/Rubrica.java
+++ Rubrica/src/rubrica/Rubrica.java
@@ -14,9 +14,13 @@ public Voce aggiungiVoce(String nome, String cognome, String telefono) throws Vo
 		throw new VoceGi‡Esiste("Voce gi‡ registrata");
 	}
 
+	
 	Voce v= new Voce(nome, cognome, telefono);
 	rubrica.put(nome + " " + cognome, v);
-
+   
+	for(Voce v1:rubrica.values()){
+	   System.out.println(v1.getNome());
+   }
 return v;
 }
 
