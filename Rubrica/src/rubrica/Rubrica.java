c35521617295f720a3ed047caf89c8cbb662f1d3
viviana <viviana_1993@live.it>
2016-10-04 11:45:07 +0200

    terzo e quarto

--- Rubrica/src/rubrica/Rubrica.java
+++ Rubrica/src/rubrica/Rubrica.java
@@ -1,12 +1,14 @@
 package rubrica;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class Rubrica {
 
 	//Mappa contentente le voci di rubrica
-private Map<String,Voce> rubrica = new TreeMap<String,Voce>();	
+private Map<String,Voce> rubrica = new TreeMap<String,Voce>();
 
 //Primo metodo aggiungi Voce
 public Voce aggiungiVoce(String nome, String cognome, String telefono) throws VoceGi‡Esiste
@@ -23,14 +25,20 @@ return v;
 
 //Secondo metodo getVoce, deve verificare che nome e cognome esistano altrimenti lancia una eccezione 
 
-public Voce getVoce(String nome, String cognome)
+public Voce getVoce (String nome, String cognome)throws VoceNonEsistente
 {
 	
+	if(!rubrica.containsKey(nome + " " + cognome)){
+		throw new VoceNonEsistente("Voce NON esistente");
+	}
+	Voce v1=rubrica.get(nome + " " + cognome);
+	return v1;
 }
 
 //Terzo metodo getTutteLeVoci, deve restitutire tutte le voci in rubrica
 public List<Voce> getListaVoci(){
-	
+	List <Voce> TutteLeVoci=new ArrayList<Voce>(rubrica.values());
+	return TutteLeVoci;
 }
 
 //quarto metodo, deve lanciare una eccezione se nome e cognome non esistono in rubric
