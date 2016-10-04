cc6e55f312fdea8609b18ae3803be4a8dc846be2
viviana <viviana_1993@live.it>
2016-10-04 11:50:21 +0200

    secondo e terzo metodo

---
+++ Rubrica/src/rubrica/VoceNonEsistente.java
@@ -0,0 +1,12 @@
+package rubrica;
+
+public class VoceNonEsistente extends Exception{
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	public VoceNonEsistente(String s){
+		super(s);
+	}
+
+}
