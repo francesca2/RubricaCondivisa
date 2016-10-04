b37e21fc0d956cd35e4cf8f17161ac95912d33b6
francesca2 <francesca.giordani88@gmail.com>
2016-10-04 09:16:00 +0200

    Ripristino

--- Rubrica/src/rubrica/Rubrica.java
+++ Rubrica/src/rubrica/Rubrica.java
@@ -8,41 +8,16 @@ public class Rubrica {
 	//Mappa contentente le voci di rubrica
 private Map<String,Voce> rubrica = new TreeMap<String,Voce>();	
 
-private int a;
-private String s;
-
 public Voce aggiungiVoce(String nome, String cognome, String telefono) throws VoceGi‡Esiste
 {	
 
 	if(rubrica.containsKey(nome + " " + cognome)){
 		throw new VoceGi‡Esiste("Voce gi‡ registrata");
 	}
-
-	
 	Voce v= new Voce(nome, cognome, telefono);
 	rubrica.put(nome + " " + cognome, v);
-
-	for(Voce v1:rubrica.values()){
-	   System.out.println(v1.getNome());
-		System.out.println("Ciao");
-   }
-
-	for(Voce v2:rubrica.values()){
-		System.out.println(v2.getCognome());
-		if(v.getNome()=="Viviana")
-		{
-			System.out.println("Il tuo nome Ë Viviana");
-		}
-
-		for(Voce v3:rubrica.values()){
-			   System.out.println(v3.getTelefono());
-				System.out.println("hello!");
-				if(v.getTelefono().equals("55555"))
-				{
-					System.out.println("Il tuo telefono Ë" + v.getTelefono());
-				}
-		   }
-
+}
+	
 	}
 return v;
 }
